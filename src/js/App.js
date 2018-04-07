06ee7d49b3fabd26a57965ca2288648018fee687
Jacob Foster <jacob.foster@stonesoup.solutions>
2018-04-06 20:43:56 -0700

    Added webpack/react.

---
+++ src/js/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <p>React here!</p>
+    </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js',
+    output: {
+        path: __dirname + '/assets',
+        filename: 'js/app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            hash: true
+        })
+    ]
+};
